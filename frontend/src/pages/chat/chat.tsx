4cdc24e28b439dc340132714fcba03249a0a74bd
maanasmahato4 <maanasmahato456@gmail.com>
2024-03-16 00:10:04 +0545

    [update]: chat working

--- frontend/src/pages/chat/chat.tsx
+++ frontend/src/pages/chat/chat.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { User } from '../../@types/index.spec';
 import io, { Socket } from 'socket.io-client';
 import styles from './chat.module.css';
@@ -7,7 +7,7 @@ interface ChatComponentProps {
 }
 
 interface Message {
-	message: string;
+	text: string;
 	user: string;
 }
 
@@ -16,17 +16,28 @@ let socket: Socket;
 const Chat: React.FC<ChatComponentProps> = ({ user }): React.ReactElement => {
 	const [messages, setMessages] = useState<Message[]>([]);
 	const [users, setUsers] = useState<User[]>([]);
+	const [userMessage, setUserMessage] = useState<string>('');
 	console.log(messages, users);
+
+	function sendMessage(): void {
+		socket.emit('sendMessage', userMessage);
+		setUserMessage('');
+	}
+
 	useEffect(() => {
 		socket = io('http://localhost:3000', {
 			transports: ['websocket', 'polling', 'flashsocket'],
 		});
 
-		socket.emit('join', { name: user.name, room: user.room }, (error: any) => {
-			if (error) {
-				console.error(error);
-			}
-		});
+		socket.emit(
+			'join',
+			{ name: user.name, room: user.room },
+			(error: unknown) => {
+				if (error) {
+					console.error(error);
+				}
+			},
+		);
 	}, [user]);
 
 	useEffect(() => {
@@ -38,7 +49,38 @@ const Chat: React.FC<ChatComponentProps> = ({ user }): React.ReactElement => {
 			setUsers(users);
 		});
 	}, []);
-	return <div>Chat</div>;
+	return (
+		<section>
+			<div className={styles['chat-box']}>
+				<div className={styles['chat-header']}>
+					<span>{user.name}</span>
+				</div>
+				<div className={styles['chat-body']}>
+					{messages.map((message, idx) => {
+						return (
+							<div key={idx}>
+								{message.user}: {message.text}
+							</div>
+						);
+					})}
+				</div>
+				<div className={styles['chat-footer']}>
+					<span>
+						<input
+							type='text'
+							value={userMessage}
+							onChange={(e: ChangeEvent<HTMLInputElement>) =>
+								setUserMessage(e.target.value)
+							}
+						/>
+						<button type='button' onClick={sendMessage}>
+							Send
+						</button>
+					</span>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Chat;
