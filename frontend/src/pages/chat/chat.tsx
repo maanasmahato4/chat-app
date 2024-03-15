a8969a373df06c8c476d7eb3ed959bebb9a0af23
maanasmahato4 <maanasmahato456@gmail.com>
2024-03-15 18:15:22 +0545

    [new]: pages setup

---
+++ frontend/src/pages/chat/chat.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './chat.module.css';
+
+const Chat: React.FC = (): React.ReactElement => {
+	return <div></div>;
+};
+
+export default Chat;
---
+++ frontend/src/pages/join/join.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './join.module.css';
+
+const Join: React.FC = (): React.ReactElement => {
+	return <div></div>;
+};
+
+export default Join;
